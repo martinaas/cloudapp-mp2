d4f829dd6b06a9a0f4ed749b49660a12b1764e01
martinaas <maregu@gmail.com>
2015-09-20 11:15:03 +0100

    Initial changes

--- TitleCount.java
+++ TitleCount.java
@@ -94,16 +94,15 @@ public class TitleCount extends Configured implements Tool {
             }
         }
     }
-}
 
-public static class TitleCountReduce extends Reducer<Text, IntWritable, Text, IntWritable> {
-    @Override
-    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-        int sum = 0;
-        for (IntWritable val : values) {
-            sum += val.get();
+    public static class TitleCountReduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+        @Override
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            context.write(key, new IntWritable(sum));
         }
-        context.write(key, new IntWritable(sum));
     }
-}
 }
